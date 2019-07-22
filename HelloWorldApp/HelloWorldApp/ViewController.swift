8e50c4663b20b4541bb903a7edd55ca64d5a9012
SviluppoCodice <51750512+Sviluppocodice@users.noreply.github.com>
2019-07-22 14:57:51 +0200

    Add files via upload

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Federico Barbieri on 13/05/2019.
+//  Copyright © 2019 Federico Barbieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //self.titleLabel.text = "Ciao Dev"
+    }
+
+
+}
+
